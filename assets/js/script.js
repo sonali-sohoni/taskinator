d5fc507b2c02c22256ceeddbe3f373fdb646244b
sonali_sohoni <ssohoni@camsbycbs.com>
2021-09-10 13:58:22 -0500

    code refactor complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
 	event.preventDefault();
 	var task_name_input = document.querySelector("input[name='task-name']").value;
 	var task_type_input = document.querySelector(
 		"select[name = 'task-type']"
 	).value;
+	var taskDataObject = new Object();
+	taskDataObject["task_name"] = task_name_input;
+	taskDataObject["task_type"] = task_type_input;
+	console.log(taskDataObject);
+	createTaskEl(taskDataObject);
+	// var listItemEl = document.createElement("li");
+	// listItemEl.className = "task-item";
+	// var taskInfoEl = document.createElement("div");
+	// taskInfoEl.innerHTML =
+	// 	"<h3 class = 'task-name'>" +
+	// 	task_name_input +
+	// 	"</h3> <span class ='task-type'>" +
+	// 	task_type_input +
+	// 	"</span>";
+	// listItemEl.appendChild(taskInfoEl);
+	// tasksToDoEl.appendChild(listItemEl);
+};
 
+var createTaskEl = function (obj) {
+	var task_name_input = obj["task_name"];
+	var task_type_input = obj["task_type"];
 	var listItemEl = document.createElement("li");
 	listItemEl.className = "task-item";
 	var taskInfoEl = document.createElement("div");
@@ -19,4 +39,5 @@ var createTaskHandler = function (event) {
 	listItemEl.appendChild(taskInfoEl);
 	tasksToDoEl.appendChild(listItemEl);
 };
-formEl.addEventListener("submit", createTaskHandler);
+
+formEl.addEventListener("submit", taskFormHandler);
