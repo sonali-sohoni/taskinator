a3cf9b2895698dea27ab761c3d5afa48c51f1dc0
sonali_sohoni <ssohoni@camsbycbs.com>
2021-09-10 08:59:07 -0500

    Task add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
-	console.log(event);
+	event.preventDefault();
+	var task_name_input = document.querySelector("input[name='task-name']").value;
+	var task_type_input = document.querySelector(
+		"select[name = 'task-type']"
+	).value;
+
 	var listItemEl = document.createElement("li");
 	listItemEl.className = "task-item";
-	listItemEl.textContent = "This is a new task.";
+	var taskInfoEl = document.createElement("div");
+	taskInfoEl.innerHTML =
+		"<h3 class = 'task-name'>" +
+		task_name_input +
+		"</h3> <span class ='task-type'>" +
+		task_type_input +
+		"</span>";
+	listItemEl.appendChild(taskInfoEl);
 	tasksToDoEl.appendChild(listItemEl);
 };
 formEl.addEventListener("submit", createTaskHandler);
