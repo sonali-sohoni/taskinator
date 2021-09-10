b3216fa8f37027532f5ad8851b36c2be4e24eea9
sonali_sohoni <ssohoni@camsbycbs.com>
2021-09-10 14:34:26 -0500

    add task functionality completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,18 @@ var taskFormHandler = function (event) {
 	var task_type_input = document.querySelector(
 		"select[name = 'task-type']"
 	).value;
-	var taskDataObject = new Object();
-	taskDataObject["task_name"] = task_name_input;
-	taskDataObject["task_type"] = task_type_input;
+	document.formEl.reset();
+	if (!task_name_input || !task_type_input) {
+		alert("Please fill out the form");
+		return;
+	}
+	var taskDataObject = {
+		task_name: task_name_input,
+		task_type: task_type_input,
+	};
+
 	console.log(taskDataObject);
 	createTaskEl(taskDataObject);
-	// var listItemEl = document.createElement("li");
-	// listItemEl.className = "task-item";
-	// var taskInfoEl = document.createElement("div");
-	// taskInfoEl.innerHTML =
-	// 	"<h3 class = 'task-name'>" +
-	// 	task_name_input +
-	// 	"</h3> <span class ='task-type'>" +
-	// 	task_type_input +
-	// 	"</span>";
-	// listItemEl.appendChild(taskInfoEl);
-	// tasksToDoEl.appendChild(listItemEl);
 };
 
 var createTaskEl = function (obj) {
